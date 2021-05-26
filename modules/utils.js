5f960c0ef57f8cb02045ea621a4add3bcad82d64
amandeepmittal <amandeepmittal@live.com>
2021-05-26 16:18:58 +0530

    one signal set up

--- modules/utils.js
+++ modules/utils.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
 
 const YourApp = () => {
   return (
@@ -7,7 +7,7 @@ const YourApp = () => {
       <Text style={styles.text}>Welcome to your brand new app!</Text>
     </View>
   )
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 20
-  },
+  }
 })
 
 const YourAppModule = {
@@ -27,54 +27,54 @@ const YourAppModule = {
 
 const sortNavigators = (a, b) => {
   if (a.hasOwnProperty("navigator") && b.hasOwnProperty("navigator")) {
-    return 0;
+    return 0
   } else if (a.hasOwnProperty("navigator")) {
-    return -1;
+    return -1
   } else {
-    return 1;
+    return 1
   }
 }
 
 const sortMenu = (a, b) => {
   if (a.title == "App Menu") {
-    return -1;
+    return -1
   } else {
-    return 0;
+    return 0
   }
 }
 
 const validate = (mod, prop) => {
-  return mod.hasOwnProperty(prop);
-};
+  return mod.hasOwnProperty(prop)
+}
 
-export const getModules = (manifest) => {
-  let modules = [];
+export const getModules = manifest => {
+  let modules = []
   for (const [name, definition] of Object.entries(manifest)) {
     if (definition && validate(definition, "title")) {
       modules.push(definition)
     } else {
-      let title = name.replace(/([A-Z])/g, " $1");
-      title = title.charAt(0).toUpperCase() + title.slice(1);
+      let title = name.replace(/([A-Z])/g, " $1")
+      title = title.charAt(0).toUpperCase() + title.slice(1)
       modules.push({
         title: title,
         navigator: definition
-      });
+      })
     }
   }
-  modules = modules.sort(sortNavigators);
-  modules = modules.sort(sortMenu);
+  modules = modules.sort(sortNavigators)
+  modules = modules.sort(sortMenu)
   if (!(modules.length && modules[0].hasOwnProperty("navigator"))) {
-    modules.splice(0, 0, YourAppModule);
+    modules.splice(0, 0, YourAppModule)
   }
-  return modules;
+  return modules
 }
 
 export function getPropertyMap(source, prop) {
-  let map = {};
+  let map = {}
   source.map(mod => {
     if (mod[prop]) {
       map[mod.title] = mod[prop]
     }
-  });
-  return map;
+  })
+  return map
 }
